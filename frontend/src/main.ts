cb7655f980375f6280aa06ca169741f6968c21ed
fernanCruz <142925007+fernanCruz@users.noreply.github.com>
2025-10-16 00:05:40 -0300

    fix: load boostrapStyles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
 async function main() {
+  async function main() {
   await bootstrapApplication(AppComponent, appConfig);
   if (typeof window !== 'undefined') {
-    // carga el JS de Bootstrap sólo en el navegador
     await import('bootstrap');
   }
+
+  const loader = document.getElementById('initial-loader');
+  if (loader) loader.remove();
+}
+void main();
+
 }
 void main();
